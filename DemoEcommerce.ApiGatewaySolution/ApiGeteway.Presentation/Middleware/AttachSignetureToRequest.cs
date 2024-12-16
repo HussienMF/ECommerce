eecda633a2bc9fd17444ccf778ed91d8faa346eb
HussienMF <hussienfatrawi@gmail.com>
2024-12-16 18:49:26 +0300

    Description of changes made

---
+++ DemoEcommerce.ApiGatewaySolution/ApiGeteway.Presentation/Middleware/AttachSignetureToRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiGeteway.Presentation.Middleware
+{
+    public class AttachSignetureToRequest(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.Headers["Api-Gateway"] = "Signed";
+            await next(context);
+        }
+    }
+}
